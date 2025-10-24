fe87c5111491680f1866232332222dfbc556d8d6
Tharun Kumar S <ariyaltharun@gmail.com>
2025-10-24 22:05:33 +0530

    fix screen width inconsistent value
    * initially screen height value is assigned to screen width variable
      this was creating problem in drawing rectangles and text with
      unexpected values
    * fix by assigning actual screen width to screen width variable
      also change the ui width params, where ever used

--- app/src/main/cpp/src/ballbalancinggame.cpp
+++ app/src/main/cpp/src/ballbalancinggame.cpp
@@ -18,7 +18,7 @@ BallBalancingGame::BallBalancingGame() {
     LOGI("Target Frames Per Second is set to: 60");
     SetTargetFPS(60);
     LOGI("Width = %d, Height = %d", SCREEN_WIDTH, SCREEN_HEIGHT);
-    screenWidth = GetScreenHeight();
+    screenWidth = GetScreenWidth();
     screenHeight = GetScreenHeight();
     gameState = GameState::START_SCREEN;
 }
@@ -26,9 +26,9 @@ BallBalancingGame::BallBalancingGame() {
 void BallBalancingGame::startScreen() {
     LOGI("Start Screen!!");
     Vector2 touch = {0.0f, 0.0f};
-    Rectangle start_option = {static_cast<float>(0.05*screenWidth), static_cast<float>(0.4*screenHeight), static_cast<float>(0.37*screenWidth), static_cast<float>(0.07*screenHeight)};
-    Rectangle level_option = {static_cast<float>(0.05*screenWidth), static_cast<float>(0.5*screenHeight), static_cast<float>(0.37*screenWidth), static_cast<float>(0.07*screenHeight)};
-    Rectangle quit_option = {static_cast<float>(0.05*screenWidth), static_cast<float>(0.6*screenHeight), static_cast<float>(0.37*screenWidth), static_cast<float>(0.07*screenHeight)};
+    Rectangle start_option = {static_cast<float>(0.1*screenWidth), static_cast<float>(0.4*screenHeight), static_cast<float>(0.8*screenWidth), static_cast<float>(0.07*screenHeight)};
+    Rectangle level_option = {static_cast<float>(0.1*screenWidth), static_cast<float>(0.5*screenHeight), static_cast<float>(0.8*screenWidth), static_cast<float>(0.07*screenHeight)};
+    Rectangle quit_option = {static_cast<float>(0.1*screenWidth), static_cast<float>(0.6*screenHeight), static_cast<float>(0.8*screenWidth), static_cast<float>(0.07*screenHeight)};
 
     while (gameState == GameState::START_SCREEN) {
         /* Handle Events */
@@ -53,13 +53,13 @@ void BallBalancingGame::startScreen() {
             DrawText(TextFormat("Touch at: %f, %f", touch.x, touch.y), 10, 10, 20, RED);
             // Start Option Button
             DrawRectangleRec(start_option, DARKGREEN);
-            DrawText("Start Game", 0.15*screenWidth, 0.42*screenHeight, 50, RAYWHITE);
+            DrawText("Start Game", 0.3*screenWidth, 0.42*screenHeight, 50, RAYWHITE);
             // Level Option Button
             DrawRectangleRec(level_option, DARKBLUE);
-            DrawText("Select Levels", 0.125*screenWidth, 0.52*screenHeight, 50, RAYWHITE);
+            DrawText("Select Levels", 0.25*screenWidth, 0.52*screenHeight, 50, RAYWHITE);
             // Quit Option Button
             DrawRectangleRec(quit_option, RED);
-            DrawText("Quit Game", 0.155*screenWidth, 0.62*screenHeight, 50, RAYWHITE);
+            DrawText("Quit Game", 0.33*screenWidth, 0.62*screenHeight, 50, RAYWHITE);
             // Footer Text
             DrawText("Made with  by Ariyal with Raylib", 0.1*screenWidth, 0.95*screenHeight, 25, RAYWHITE);
         }
